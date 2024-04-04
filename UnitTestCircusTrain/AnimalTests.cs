190adf11b51c9a0f021eaa111d6fef33904d8109
A.M.K. van den Herik <karlijnvandenherik@gmail.com>
2024-04-04 09:30:48 +0200

    Added the unittests

---
+++ UnitTestCircusTrain/AnimalTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Circustrain_Karlijn_vd_Herik;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCircusTrain
+{
+    [TestClass]
+    public class AnimalTests
+    {
+        [TestMethod]
+        public void IsLargeMeatEaterFriendly_ShouldReturnFalse()
+        {
+            //arrange
+            var largeMeatEater = new Animal(AnimalSize.Large, true);
+            var mediumMeatEater = new Animal(AnimalSize.Medium, true);
+            var smallMeatEater = new Animal(AnimalSize.Small, true);
+
+            var largePlantEater = new Animal(AnimalSize.Large, false);
+            var mediumPlantEater = new Animal(AnimalSize.Medium, false);
+            var smallPlantEater = new Animal(AnimalSize.Small, false);
+
+
+            //act
+            //asses
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(largeMeatEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(mediumMeatEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(smallMeatEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(largePlantEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(mediumPlantEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(smallPlantEater));
+        }
+        [TestMethod]
+        public void IsMediumMeatEaterFriendly_ShouldReturnFalse()
+        {
+            //arrange
+            var largeMeatEater = new Animal(AnimalSize.Large, true);
+            var mediumMeatEater = new Animal(AnimalSize.Medium, true);
+            var smallMeatEater = new Animal(AnimalSize.Small, true);
+
+            var largePlantEater = new Animal(AnimalSize.Large, false);
+            var mediumPlantEater = new Animal(AnimalSize.Medium, false);
+            var smallPlantEater = new Animal(AnimalSize.Small, false);
+
+
+            //act
+            //asses
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(mediumMeatEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(smallMeatEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(mediumPlantEater));
+            Assert.IsFalse(largeMeatEater.IsFriendlyAnimal(smallPlantEater));
+        }
+    }
+}
---
+++ UnitTestCircusTrain/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCircusTrain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCircusTrain")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f624943f-6c73-4fda-89fb-5b86809a2fb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
